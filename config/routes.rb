97b2d040c5b78ee0b4ef0ffc5e8de71ba257de24
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-29 15:23:06 +0000

    Finish search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'searches/search'
+  get 'search' => 'searches#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
