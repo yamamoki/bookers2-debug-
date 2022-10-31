510ae1f4e18717f641a62a8823e4410d723d06ed
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-31 06:19:13 +0000

    Add hidouki

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,15 +3,15 @@ class FavoritesController < ApplicationController
 
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     #redirect_to book_path(book)非同期
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     #redirect_to book_path(book)非同期
   end
