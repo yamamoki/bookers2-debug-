4393fce691ce0bbf6319b2f1b69a9a5c5b0cf298
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-11-03 03:13:26 +0000

    Start 7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @books = Book.includes(:favorites).sort {|a,b| b.favorites.size <=> a.favorites.size}
     @book=Book.new
     @user=current_user
   end
@@ -45,6 +45,7 @@ class BooksController < ApplicationController
     @book.destroy
     redirect_to books_path
   end
+
 
   private
 
